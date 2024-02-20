70034e1c0894e047a4780eacbd20c4182e14764b
Rasmus Craelius <rasmusalexander3000@gmail.com>
2024-02-20 20:43:39 +0100

    Testing setup
    #27

---
+++ src/test/java/com/esri/core/geometry/TesttryFastIntersectPolylinePolygon.java
@@ -0,0 +1,21 @@
+package com.esri.core.geometry;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class TesttryFastIntersectPolylinePolygon {
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    @Test
+    public void testNullAcceleration() {
+
+    }
+}
